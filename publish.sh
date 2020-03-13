6c1bb44e227dc3307c8174fe8ccd2a4ef728e1d6
Justin Ashworth <justin.ashworth@valid.com>
2020-03-13 12:24:58 -0400

    Fixes for publish script

--- publish.sh
+++ publish.sh
@@ -1,1 +1,38 @@
-dotnet nuget push ./IntervalCronGenerator.Core/bin/Debug/IntervalCronGenerator.Core.1.0.0.nupkg -k $INTERVAL_CRON_GENERATOR_KEY -s https://api.nuget.org/v3/index.json
+#!/bin/bash
+export CSPROJ_PATH="./IntervalCronGenerator.Core/"
+export PROJECT_NAME="IntervalCronGenerator.Core.csproj"
+export GIT_BRANCH=$(git branch | cut -c 3-)
+export TEST_PROJECT="./IntervalCronGeneratorTests/IntervalCronGeneratorTests.csproj"
+export NUGET_SERVER="https://api.nuget.org/v3/index.json"
+export BUILD_DIR="./build"
+
+set -euo pipefail
+
+project=$(basename -s .csproj "$CSPROJ_PATH$PROJECT_NAME")
+version=$(sed -n 's:.*<Version>\(.*\)</Version>.*:\1:p' "$CSPROJ_PATH$PROJECT_NAME")
+version_suffix='--version-suffix "pre"'
+nupkg_file=./build/$project.$version-pre.nupkg
+
+echo "Project: $project"
+echo "Branch: $GIT_BRANCH"
+echo "Testing $project version: $version"
+
+dotnet test "$TEST_PROJECT"
+
+# Nuget packages default to "pre" release unless on master
+if [ "$GIT_BRANCH" == "master" ]; then
+    echo "Building production release"
+    nupkg_file=./build/$project.$version.nupkg
+    version_suffix=''
+fi
+
+dotnet pack "$CSPROJ_PATH$PROJECT_NAME" -o "$BUILD_DIR" --include-symbols $version_suffix
+
+# Only publish when building on master or develop
+if [ "$GIT_BRANCH" == "master" ] || [ "$GIT_BRANCH" == "develop" ]; then
+
+    echo "Publishing $nupkg_file to $NUGET_SERVER"
+
+    # Publish to nuget using NUGET_SERVER and NUGET_API_KEY env variables
+    dotnet nuget push "$nupkg_file" -s "$NUGET_SERVER" -k "$INTERVAL_CRON_GENERATOR_KEY" -t 60 -n --force-english-output
+fi
